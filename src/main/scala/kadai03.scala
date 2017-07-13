be69da8788c6d86aa87013b77458f7cfd68241f8
katsuramaki taiki <llxo2_5oxll@icloud.com>
2017-07-13 15:30:12 +0900

    kadai03

--- src/main/scala/kadai03.scala
+++ src/main/scala/kadai03.scala
@@ -1,6 +1,28 @@
+
 /**
   * Created by taiki on 2017/07/13.
   */
-class kadai03 {
 
+object NumEnglishDictionary {
+  private val dictionary = Map(1 -> "one", 2 -> "two")
+
+  def translate(num: Option[Int]): Option[String] = {
+    val numInt = num match {
+      case Some(num) => num
+      case None => return None
+    }
+    this.dictionary.get(numInt) match {
+      case Some(v) => return Some(v)
+      case None => return None
+    }
+  }
+}
+
+object kadai03 {
+  def main(args: Array[String]): Unit = {
+    // Usage
+    println(NumEnglishDictionary.translate(Some(1))) // Some("one")
+    println(NumEnglishDictionary.translate(Some(3))) // None
+    println(NumEnglishDictionary.translate(None)) // None
+  }
 }
