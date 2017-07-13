4b9bd34211dbd527c7726d455f1c250381d5a9d9
katsuramaki taiki <llxo2_5oxll@icloud.com>
2017-07-13 14:16:18 +0900

    kadai01

---
+++ src/main/scala/kadai01.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by taiki on 2017/07/13.
+  */
+object kadai01 {
+  def main(args: Array[String]): Unit = {
+    // Usage
+    show(Some("hello")) // helloと出力
+    show(None) // 何も出力されない(printlnも実行しないこと)
+  }
+
+  def show(value: Option[String]): Unit = {
+    value match {
+      case Some(v) => println(v)
+      case None =>
+    }
+  }
+}
+
